e67f108df67895b93d9f21876debb2531bbfc1ea
Karim <z5209523@ad.unsw.edu.au>
2021-01-15 16:20:49 +1100

    generic commit message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello!</h1>
       <p>This site is still under production. Please wait till further notice.</p>
       <p>In the meantime, feel free to contact Karim at <a href="mailto:karimsaad47@gmail.com">karimsaad47@gmail.com</a></p>
     </div>
